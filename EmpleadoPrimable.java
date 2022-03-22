43be1de1355139eafc72e627d8459e0a5354fa02
amadorcastellanorafa <101814728+amadorcastellanorafa@users.noreply.github.com>
2022-03-22 19:21:03 +0100

    Add files via upload

---
+++ EmpleadoPrimable.java
@@ -0,0 +1,15 @@
+public class EmpleadoPrimable extends Empleado{
+    private boolean primado;
+    public EmpleadoPrimable(String n, double s) {
+        super(n, s);
+        this.primado=false;
+    }
+    public void primar(){
+        this.primado=true;
+    }
+    public boolean esPrimado(){
+        return primado;
+    }
+    
+    
+}
